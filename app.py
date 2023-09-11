0bdfe2034f2cb128ad753c200dd0eaaa12083ab9
Ehinmore Adebameno <109996808+Adeola19B@users.noreply.github.com>
2023-09-11 21:57:53 +0000

    another commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+import datetime
+app = Flask(__name__)
+
+@app.route('/details', methods=['GET'])
+def my_endpoint():
+    data = {
+        "slack_name": "Ehinmore Adeola Adebameno",
+        "current_day": "Monday" ,
+        "utc-time":datetime.datetime.today().isoformat(),
+        "track": "backend",
+        "github_file_url": "https://github.com/Adeola19B/hng/app.py",
+        "github_repo_url": "https://github.com/Adeola19B/hng",
+        "status_code": 200
+    }
+    response = jsonify(data)
+
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
+
+if __name__ == "__main__":
+    app.run()
