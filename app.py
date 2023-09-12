ca1f3ebd15b910ccd2e6aaf013f4a1329161f6a4
Ehinmore Adebameno <109996808+Adeola19B@users.noreply.github.com>
2023-09-12 12:20:37 +0000

    added~

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ app = Flask(__name__)
 def myendpoint():
     now = datetime.utcnow()
     current = now.strftime('%A')
-    utc = now.strftime("%Y-%m-%dT%H:%M:%SZ")
-    
+    utc = now.strftime('%Y-%m-%dT%H:%M:%SZ')
+    valid_time_range = now + timedelta(hours=-2), now + timedelta(hours=2)
+    if ('since' in request.args) and ('until' in request.args):
+        try:
+            since = datetime.fromisoformat(request.args['since'])
+            until = datetime.fromisoformat(request.args['until'])
+        except ValueError:
+            return jsonify({'error': 'Invalid date format'}), 400
+        if (since < valid_time_range[0]) or (until > valid_time_range[1]):
+            return jsonify({'error': 'Date range is out of valid time range'}), 400
+
     data = {
         "slack_name": "Adeola19B",
         "current_day": current,
